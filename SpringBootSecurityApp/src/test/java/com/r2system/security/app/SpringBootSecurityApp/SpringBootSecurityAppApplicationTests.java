6618455bd17900fe55c1b0d5f79dc95ee7fe5117
Simbak Ramirez Gonzalez <simbak.ramirez@gmail.com>
2022-03-22 23:28:01 -0600

    01- Init App
    Inicio de proyecto con Loombok y WebApi

---
+++ SpringBootSecurityApp/src/test/java/com/r2system/security/app/SpringBootSecurityApp/SpringBootSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r2system.security.app.SpringBootSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
