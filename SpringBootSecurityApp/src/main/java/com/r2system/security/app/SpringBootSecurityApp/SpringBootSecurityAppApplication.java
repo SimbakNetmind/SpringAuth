354231a70f8922919d644c1e0ef92a337346c041
Simbak Ramirez Gonzalez <simbak.ramirez@gmail.com>
2022-03-23 12:28:15 -0600

    04- Conexion JDBC

--- SpringBootSecurityApp/src/main/java/com/r2system/security/app/SpringBootSecurityApp/SpringBootSecurityAppApplication.java
+++ SpringBootSecurityApp/src/main/java/com/r2system/security/app/SpringBootSecurityApp/SpringBootSecurityAppApplication.java
@@ -1,5 +1,6 @@
 package com.r2system.security.app.SpringBootSecurityApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,15 +8,30 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootSecurityAppApplication {
+public class SpringBootSecurityAppApplication  implements CommandLineRunner {
 
 //   @Bean
 //	public PasswordEncoder passwordEncoder(){
 //		return  new BCryptPasswordEncoder();
 //	}
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		String password = "12345";
+
+		for(int i=0; i<2; i++) {
+			String bcryptPassword = passwordEncoder().encode(password);
+			System.out.println(bcryptPassword);
+		}
+
+	}
 }
--- SpringBootSecurityApp/src/main/java/com/r2system/security/app/SpringBootSecurityApp/config/security/SecurityConfig.java
+++ SpringBootSecurityApp/src/main/java/com/r2system/security/app/SpringBootSecurityApp/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.r2system.security.app.SpringBootSecurityApp.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,28 +8,46 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 //import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import javax.sql.DataSource;
 
 @Component
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
-//    @AutoConfigureOrder
-//    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+
+        //Autenticacion Virtual
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+//
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(users.username("simbak").password("123123").roles("ADMIN"))
+//                .withUser(users.username("juan").password("123123").roles("MEMBER"));
 
 
-        auth.inMemoryAuthentication()
-                .withUser(users.username("simbak").password("123123").roles("ADMIN"))
-                .withUser(users.username("juan").password("123123").roles("MEMBER"));
+        //Conexion JDBC
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
+
     }
 
     @Override
@@ -39,14 +58,19 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
              http.authorizeRequests()
                      //.antMatchers("/api/auth/**").hasRole("ADMIN")
-                     .antMatchers(HttpMethod.GET,"/api/admin").hasRole("ADMIN")
-                     .antMatchers(HttpMethod.GET,"/api/member").hasRole("MEMBER")
-                     //.antMatchers(HttpMethod.GET,"/api/swagger-ui/**").permitAll()
+                     .antMatchers(HttpMethod.GET,"/api/admin","/api/both").hasRole("ADMIN")
+                     .antMatchers(HttpMethod.GET,"/api/member").hasRole("USER")
                      .anyRequest().authenticated()
                      .and()
                      .formLogin()
                      .and()
                      .exceptionHandling().accessDeniedPage("/error/unauthorize");
+
+                 http
+                .logout(logout -> logout
+                        .logoutUrl("/basic/logout")
+                        .addLogoutHandler(new SecurityContextLogoutHandler())
+                );
 
     }
 
